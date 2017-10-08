ac7cdd70ca20b97db73ee9f4b081e20768e33f52
AurelK <32571174+AurelK@users.noreply.github.com>
2017-10-08 21:23:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Soup 2.0</title>
+<link rel="stylesheet" href="design.css">
+</head>
+<body>
+
+<?php 
+		include ('nav.php');
+	?>
+	
+	<div id="link">
+	
+	 <img src="smiley.gif" alt="Smiley face" height="42" width="42"> 
+	
+	</div>
+
+</body>
